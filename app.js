9bf4b0a2f7ff16b96d5af39721b3b89aae14e5f4
Areeya <amyschclass@gmail.com>
2024-03-16 02:12:54 +0700

    link app.js, listItem.js, list.ejs

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const mysql = require('mysql2');
 const listItem = require("./model/listItem")
 const db = require("./config/db.js");
 
+const { ProductItemsModel } = require("./model/listItem");
+
 const app = express();
 app.set("view engine", "ejs");
 
@@ -47,16 +49,31 @@ app.get('/homepage', function(req, res) {
     res.sendFile(path.join(__dirname, 'public', 'html', 'homepage.html'));
 });
 
-app.get('/list', async function(req, res) {
-    // Populate the tables with data if they are empty
-    const snackItems = new ProductItemsModel("snack_items");
-    await snackItems.defineProductItems();
+const snackItems = new ProductItemsModel("snack_items");
+const dessertItems = new ProductItemsModel("dessert_items");
+const meatItems = new ProductItemsModel("meat_items");
+const seafoodItems = new ProductItemsModel("seafood_items");
+const fruitItems = new ProductItemsModel("fruit_items");
+const vegetableItems = new ProductItemsModel("vegetable_items");
 
-    // Fetch the list of snack names from the database
+(async () => {
+  await snackItems.defineProductItems();
+  await dessertItems.defineProductItems();
+  await meatItems.defineProductItems();
+  await seafoodItems.defineProductItems();
+  await fruitItems.defineProductItems();
+  await vegetableItems.defineProductItems();
+})();
+
+app.get('/list/snacks', async function(req, res) {
     const snackNames = await snackItems.getProductItemsName();
+    res.render('list', { listTitle: "snack", items: snackNames });
+});
 
-    // Render the 'list.ejs' template and pass the snack names as data
-    res.render('list', { items: snackNames });
+// Handle the '/list' route for meats
+app.get('/list/meats', async function(req, res) {
+    const meatNames = await meatItems.getProductItemsName();
+    res.render('list', { listTitle: "meat", items: meatNames });
 });
 
 // Start the server on port 3000
