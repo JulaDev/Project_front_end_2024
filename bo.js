fb69db7a57d52fd0d1656655993c87358c6ff73e
Pipe-lines <pipe-lines>
2024-03-15 07:28:42 +0700

    Removed some package to prevent error.
    It will be install when working on the login part.

--- bo.js
+++ bo.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const db = require("./db");
-const session = require('express-session')
-const axios = require("axios");
 
 const app = express();
 
